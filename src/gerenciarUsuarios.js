cd23c47309c1f419bbe7445621868763e0ea38d7
Danielle <daniellesittalopes@gmail.com>
2025-09-09 22:56:20 -0300

    Mudei a lista de nomes de usuários para tornarem-se objetos

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,12 +1,32 @@
 //criar uma lista de usuários. os [] indica lista e as aspas simples indica texto
-const usuarios = ['Matheus', 'Marcos', 'Lucas', 'João'];
+const usuarios = [
+    {
+        Nome:'Matheus',
+        email: 'matheus@gmail.com'
+    },
+
+    { 
+        Nome:'Marcos',
+        email: 'marcos@gmail.com'
+    },
+    
+    { 
+        Nome:'Lucas',
+        email: 'lucas@gmail.com'
+    },
+                    
+    { 
+        Nome:'João',
+        email: 'joao@gmail.com'
+    }
+];
 
 function retornarUsuarios() {
     return usuarios;
 };
 
-function adicionarNovoUsuario(nome) {
-    usuarios.push(nome);
+function adicionarNovoUsuario(usuario) {
+    usuarios.push(usuario);
 }
 
 module.exports = {
